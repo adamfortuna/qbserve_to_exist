4af3d805878a7c425df895eba94f8a94edfd3c4f
Adam Fortuna <adam@fortuna.name>
2021-01-01 11:49:15 -0700

    Combine duplicate entries when they span over multiple tables due to timezone differences

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3').verbose(),
       moment = require('moment')
 const DATABASE_URL = process.env.QBSERVE_DATABASE_URL,
       EXIST_ACCESS_TOKEN = process.env.EXIST_ACCESS_TOKEN,
-      START_DATE = moment(process.env.EXIST_START_DATE).startOf('day').format('X')
+      START_DATE = process.env.EXIST_START_DATE ? moment(process.env.EXIST_START_DATE).startOf('day').format('X') : moment().subtract(7, 'days').format('X')
 
 axios.defaults.headers.common['Authorization'] = `Bearer ${EXIST_ACCESS_TOKEN}`;
 
@@ -55,7 +55,6 @@ function flattenActivities(activities) {
 
 function filterActivitiesByDate(activities) {
   console.log('filterActivitiesByDate')
-
   return activities.filter((activity) => {
     return activity.start_time >= START_DATE
   })
@@ -70,6 +69,25 @@ function convertActivitiesToExist(activities) {
       value: parseInt(activity.duration / 60)
     }
   })
+}
+
+// Arr[Ob] => Ob[date-productivity] => Arr[Ob]
+// Arr[Ob] => Arr[Ob-No-dupes] + Arr[Ob-de-dupes]
+function combineDuplicateActivities(activities) {
+  console.log('combineDuplicateActivities')
+  var noDupes = activities.filter((a) => findAllByActivity(a, activities).length == 1)
+  var dupes = activities.filter((a) => findAllByActivity(a, activities).length > 1)
+
+  var deDupes = dupes.reduceRight((all, activity) => {
+    all[`${activity.date}-${activity.name}`] = all[`${activity.date}-${activity.name}`] || { date: activity.date, name: activity.name, value: 0 }
+    all[`${activity.date}-${activity.name}`].value = all[`${activity.date}-${activity.name}`].value + activity.value
+    return all
+  }, {})
+
+  return [noDupes, Object.values(deDupes)].flat()
+}
+function findAllByActivity(activity, activities) {
+  return activities.filter((a) => (activity.date == a.date) && (activity.name == a.name))
 }
 
 function acquireExistFields(activities) {
@@ -104,14 +122,19 @@ function sendActivitiesToExist(activities) {
   return Promise.all(promises)
 }
 
+function echo(activities) {
+  console.log('echo:', activities)
+  return activities
+}
+
 var data = [], sql
 knex.raw(HISTORY_TABLES)
   .then(parseTableNames)
-  // .then((tables) => { console.log('tables', tables); return tables; })
   .then(lookupActitiesByHistoryTable)
   .then(flattenActivities)
   .then(filterActivitiesByDate)
   .then(convertActivitiesToExist)
+  .then(combineDuplicateActivities)
   .then(acquireExistFields)
   .then(sendActivitiesToExist)
   .catch((e) => console.log("Error:", e))
